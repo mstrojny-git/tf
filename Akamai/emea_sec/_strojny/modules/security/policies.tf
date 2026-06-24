e3bd1a5fa1becdcc6cde684593c36ffbc9f42f12
Marek Strojny <mstrojny@akamai.com>
2026-06-24 14:37:48 +0200

    Fix: Usunięcie .terraform oraz emea_sec/_strojny z repozytorium

--- Akamai/emea_sec/_strojny/modules/security/policies.tf
+++
@@ -1,7 +0,0 @@
-resource "akamai_appsec_security_policy" "main" {
-  config_id              = local.config_id
-  default_settings       = true
-  security_policy_name   = "Main"
-  security_policy_prefix = "0001"
-}
-
--- Akamai/emea_sec/_strojny/modules/security/slow-post.tf
+++
@@ -1,9 +0,0 @@
-// Slow Post Protection
-resource "akamai_appsec_slow_post" "main" {
-  config_id                  = local.config_id
-  security_policy_id         = akamai_appsec_slowpost_protection.main.security_policy_id
-  slow_rate_action           = "abort"
-  slow_rate_threshold_rate   = 10
-  slow_rate_threshold_period = 60
-}
-
